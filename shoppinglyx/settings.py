7a9cb8b6a068bd89b981051eb6bc90564a8800f4
Shubham Kumar <95858496+shubhamkr3011@users.noreply.github.com>
2023-10-04 16:33:25 +0530

    Update settings.py

--- shoppinglyx/settings.py
+++ shoppinglyx/settings.py
@@ -119,6 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static"] # new
+STATIC_ROOT = BASE_DIR / "staticfiles" # new
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 LOGIN_REDIRECT_URL = '/profile/'
