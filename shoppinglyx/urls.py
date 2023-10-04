c1faa1a71c7bb52b061ad35492a0be6205b7972f
Shubham Kumar <95858496+shubhamkr3011@users.noreply.github.com>
2023-10-04 16:34:59 +0530

    Update urls.py

--- shoppinglyx/urls.py
+++ shoppinglyx/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns # new
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),
 ]
+
+urlpatterns += staticfiles_urlpatterns() # new
+
